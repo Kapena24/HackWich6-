b1e549c5d3afd05c00d366fb7ece1b83004cef7b
Kapena Kaaihue <142773521+Kapena24@users.noreply.github.com>
2023-10-10 12:44:22 -1000

    Initial Commit

---
+++ HackWich6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HackWich6
+//
+//  Created by Kapena Kaaihue on 10/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
