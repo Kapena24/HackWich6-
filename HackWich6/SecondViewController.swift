ed7892b81ba484d663565a538b657feeea617640
Kapena Kaaihue <142773521+Kapena24@users.noreply.github.com>
2023-10-10 13:15:02 -1000

    1-4 completed

---
+++ HackWich6/SecondViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SecondViewController.swift
+//  HackWich6
+//
+//  Created by Kapena Kaaihue on 10/10/23.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    // Your code for the second View Controller
+}
